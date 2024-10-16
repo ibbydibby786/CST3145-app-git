25dc939f4e81352400bf3d723bc5c94f3de14fa8
ibbydibby786 <ma3643@live.mdx.ac.uk>
2024-10-16 04:00:09 +0100

    product.js updated for after school app

--- products.js
+++ products.js
@@ -1,45 +1,45 @@
 let products = [
     {
       "id": 1001,
-      "title": "Cat Food, 25lb bag",
-      "description": "A 25 pound bag of <em>irresistible</em>, organic goodness for your cat.",
-      "price": 20.00,
-      "image": "images/product-fullsize.png",
+      "title": "Math",
+      "description": "London",
+      "price": 100.00,
+      "image": "images//maths.png",
       "availableInventory": 10,
       "rating": 2
     },
     {
       "id": 1002,
-      "title": "Yarn",
-      "description": "Yarn your cat can play with for a very <strong>long</strong> time!",
-      "price": 2.99,
-      "image": "images/yarn.jpg",
+      "title": "Math",
+      "description": "Oxford",
+      "price": 100.00,
+      "image": "images//maths.png",
       "availableInventory": 7,
       "rating": 3
     },
     {
       "id": 1003,
-      "title": "Kitty Litter",
-      "description": "Premium kitty litter for your cat.",
-      "price": 11.00,
+      "title": "English",
+      "description": "London",
+      "price": 100.00,
       "image": "images/cat-litter.jpg",
       "availableInventory": 99,
       "rating": 4
     },
     {
       "id": 1004,
-      "title": "Cat House",
-      "description": "A place for your cat to play!",
-      "price": 7.99,
+      "title": "English",
+      "description": "York",
+      "price": 80.00,
       "image": "images/cat-house.jpg",
       "availableInventory": 11,
       "rating": 5
     },
     {
       "id": 1005,
-      "title": "Laser Pointer",
-      "description": "Drive your cat crazy with this <em>amazing</em> product.",
-      "price": 49.99,
+      "title": "Music",
+      "description": "Bristol",
+      "price": 90.00,
       "image": "images/laser-pointer.jpg",
       "availableInventory": 25,
       "rating": 4
