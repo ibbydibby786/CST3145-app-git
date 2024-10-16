f0d0eba7812f0d8cf83762972f2c3469209f4092
ibbydibby786 <ma3643@live.mdx.ac.uk>
2024-10-16 22:47:01 +0100

    changes made to subjects and spaces added

--- products.js
+++ products.js
@@ -1,39 +1,43 @@
 let products = [
     {
       "id": 1001,
-      "title": "Math",
+      "title": "Maths",
       "description": "London",
-      "price": 100.00,
+      "price": 30.00,
       "image": "images//maths.png",
       "availableInventory": 10,
-      "rating": 2
+       "availableSpace": 10
+      
     },
     {
       "id": 1002,
-      "title": "Math",
+      "title": "English",
       "description": "Oxford",
       "price": 100.00,
       "image": "images//maths.png",
       "availableInventory": 7,
-      "rating": 3
+      "availableSpace": 10
+      
     },
     {
       "id": 1003,
-      "title": "English",
-      "description": "London",
-      "price": 100.00,
+      "title": "Science",
+      "description": "Luton",
+      "price": 60.00,
       "image": "images/cat-litter.jpg",
       "availableInventory": 99,
-      "rating": 4
+      "availableSpace": 10
+      
     },
     {
       "id": 1004,
-      "title": "English",
+      "title": "Religious Studies",
       "description": "York",
       "price": 80.00,
       "image": "images/cat-house.jpg",
       "availableInventory": 11,
-      "rating": 5
+      "availableSpace": 10
+      
     },
     {
       "id": 1005,
@@ -42,7 +46,58 @@ let products = [
       "price": 90.00,
       "image": "images/laser-pointer.jpg",
       "availableInventory": 25,
-      "rating": 4
-    }
+      "availableSpace": 10
+      
+    },
+     {
+      "id": 1006,
+      "title": "PE",
+      "description": "Bedford",
+      "price": 30.00,
+      "image": "images//maths.png",
+      "availableInventory": 10,
+       "availableSpace": 10
+      
+    },
+    {
+        "id": 1007,
+        "title": "IT",
+        "description": "Blackburn",
+        "price": 30.00,
+        "image": "images//maths.png",
+        "availableInventory": 10,
+         "availableSpace": 10
+        
+      },
+      {
+        "id": 1008,
+        "title": "Business studies",
+        "description": "Leicstershire",
+        "price": 30.00,
+        "image": "images//maths.png",
+        "availableInventory": 10,
+         "availableSpace": 10
+        
+      },
+      {
+        "id": 1009,
+        "title": "Art",
+        "description": "Lancaster",
+        "price": 30.00,
+        "image": "images//maths.png",
+        "availableInventory": 10,
+         "availableSpace": 10
+        
+      },
+      {
+        "id": 1010,
+        "title": "History",
+        "description": "Sheffield",
+        "price": 30.00,
+        "image": "images//maths.png",
+        "availableInventory": 10,
+         "availableSpace": 10
+        
+      },
   ];
   
