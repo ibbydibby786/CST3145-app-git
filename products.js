683ea3137ff09d4fdb19a112cca0b02ca86f8947
ibbydibby786 <ma3643@live.mdx.ac.uk>
2024-12-01 22:35:47 +0000

    changes made to html for render

--- products.js
+++ products.js
@@ -1,103 +1,92 @@
 let products = [
-    {
-      "id": 1001,
-      "title": "Maths",
-      "description": "London",
-      "price": 100.00,
-      "image": "images/maths.png",
-      "availableInventory": 10,
-       "availableSpace": 5
-      
-    },
-    {
-      "id": 1002,
-      "title": "English",
-      "description": "Oxford",
-      "price": 90.00,
-      "image": "images/english.png",
-      "availableInventory": 10,
-      "availableSpace": 5
-      
-    },
-    {
-      "id": 1003,
-      "title": "Science",
-      "description": "Luton",
-      "price": 80.00,
-      "image": "images/science.png",
-      "availableInventory": 10,
-      "availableSpace": 5
-      
-    },
-    {
-      "id": 1004,
-      "title": "Religious Studies",
-      "description": "York",
-      "price": 70.00,
-      "image": "images/religious studies.png",
-      "availableInventory": 10,
-      "availableSpace": 5
-      
-    },
-    {
-      "id": 1005,
-      "title": "Music",
-      "description": "Bristol",
-      "price": 60.00,
-      "image": "images/music.png",
-      "availableInventory": 10,
-      "availableSpace": 5
-      
-    },
-     {
-      "id": 1006,
-      "title": "PE",
-      "description": "Bedford",
-      "price": 50.00,
-      "image": "images/pe.png",
-      "availableInventory": 10,
-       "availableSpace": 5
-      
-    },
-    {
-        "id": 1007,
-        "title": "IT",
-        "description": "Blackburn",
-        "price": 40.00,
-        "image": "images/it.png",
-        "availableInventory": 10,
-         "availableSpace": 5
-        
-      },
-      {
-        "id": 1008,
-        "title": "Business studies",
-        "description": "Leicstershire",
-        "price": 30.00,
-        "image": "images/business studies.png",
-        "availableInventory": 10,
-         "availableSpace": 5
-        
-      },
-      {
-        "id": 1009,
-        "title": "Art",
-        "description": "Lancaster",
-        "price": 20.00,
-        "image": "images/art.png",
-        "availableInventory": 10,
-         "availableSpace": 5
-        
-      },
-      {
-        "id": 1010,
-        "title": "History",
-        "description": "Sheffield",
-        "price": 10.00,
-        "image": "images/history.png",
-        "availableInventory": 10,
-         "availableSpace": 5
-        
-      },
-  ];
-  
+  {
+      id: 1001,
+      title: "Maths",
+      description: "London",
+      price: 100.00,
+      image: "images/maths.png",
+      availableInventory: 10,
+      availableSpace: 5
+  },
+  {
+      id: 1002,
+      title: "English",
+      description: "Oxford",
+      price: 90.00,
+      image: "images/english.png",
+      availableInventory: 10,
+      availableSpace: 5
+  },
+  {
+      id: 1003,
+      title: "Science",
+      description: "Luton",
+      price: 80.00,
+      image: "images/science.png",
+      availableInventory: 10,
+      availableSpace: 5
+  },
+  {
+      id: 1004,
+      title: "Religious Studies",
+      description: "York",
+      price: 70.00,
+      image: "images/religious studies.png",
+      availableInventory: 10,
+      availableSpace: 5
+  },
+  {
+      id: 1005,
+      title: "Music",
+      description: "Bristol",
+      price: 60.00,
+      image: "images/music.png",
+      availableInventory: 10,
+      availableSpace: 5
+  },
+  {
+      id: 1006,
+      title: "PE",
+      description: "Bedford",
+      price: 50.00,
+      image: "images/pe.png",
+      availableInventory: 10,
+      availableSpace: 5
+  },
+  {
+      id: 1007,
+      title: "IT",
+      description: "Blackburn",
+      price: 40.00,
+      image: "images/it.png",
+      availableInventory: 10,
+      availableSpace: 5
+  },
+  {
+      id: 1008,
+      title: "Business studies",
+      description: "Leicstershire",
+      price: 30.00,
+      image: "images/business studies.png",
+      availableInventory: 10,
+      availableSpace: 5
+  },
+  {
+      id: 1009,
+      title: "Art",
+      description: "Lancaster",
+      price: 20.00,
+      image: "images/art.png",
+      availableInventory: 10,
+      availableSpace: 5
+  },
+  {
+      id: 1010,
+      title: "History",
+      description: "Sheffield",
+      price: 10.00,
+      image: "images/history.png",
+      availableInventory: 10,
+      availableSpace: 5
+  },
+];
